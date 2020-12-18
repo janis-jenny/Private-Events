92965c5b1a39a7b1750da83688e27ab38af0da8d
janis-jenny <pcardenasvm@gmail.com>
2020-12-17 20:59:25 -0500

    add navigation links in the navbar w helpers

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,13 @@
 module SessionsHelper
+  attr_writer :so_button, :si_button, :su_button
+  attr_reader :so_button, :si_button, :su_button
+  def u_signin
+    if !current_user
+      @su_button = link_to 'Sign Up', new_user_path, class: 'btn btn-info' 
+      @si_button = link_to 'Log in', login_path, class: 'btn btn-primary'
+    else
+      @so_button = link_to 'Log out', logout_path, class: 'btn btn-primary'
+      @welcome = "Welcome #{current_user.name}!"
+    end
+  end
 end
